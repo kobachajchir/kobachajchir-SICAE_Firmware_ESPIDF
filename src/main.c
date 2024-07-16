eae730a637075d33ff78370e1dd7b2dc69ba7060
kobachajchir <kobachajchir2013@gmail.com>
2024-07-16 20:09:14 -0300

    Working Hello World

--- src/main.c
+++ src/main.c
@@ -1,160 +1,129 @@
-#include <string.h>
+#include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "freertos/event_groups.h"
-#include "esp_system.h"
-#include "esp_wifi.h"
-#include "esp_event.h"
+#include "driver/i2c.h"
 #include "esp_log.h"
-#include "nvs_flash.h"
-#include "esp_mac.h"
-#include "lwip/err.h"
-#include "lwip/sys.h"
+#include <unistd.h>
 
-/* WiFi Configuration */
-#define CONFIG_ESP_WIFI_SSID "Koba"
-#define CONFIG_ESP_WIFI_PASSWORD "koba1254"
-#define CONFIG_ESP_MAXIMUM_RETRY 5
+#define I2C_MASTER_SCL_IO GPIO_NUM_22     /*!< GPIO number for I2C master clock */
+#define I2C_MASTER_SDA_IO GPIO_NUM_21    /*!< GPIO number for I2C master data  */
+#define I2C_MASTER_NUM I2C_NUM_0 /*!< I2C port number for master dev */
+#define I2C_MASTER_FREQ_HZ 100000 /*!< I2C master clock frequency */
+#define I2C_MASTER_TX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
 
-#define CONFIG_SOFTAP_SSID "ESP32AP"
-#define CONFIG_SOFTAP_PASSWORD "ap123456"
-#define CONFIG_SOFTAP_CHANNEL 1
-#define CONFIG_MAX_STA_CONN 4
+#define SLAVE_ADDRESS_LCD 0x27 /*!< I2C address of the PCF8574T chip */
 
-/* FreeRTOS event group to signal when we are connected */
-static EventGroupHandle_t s_wifi_event_group;
+static const char *TAG = "i2c_lcd";
 
-/* The event group allows multiple bits for each event, but we only care about two events:
- * - we are connected to the AP with an IP
- * - we failed to connect after the maximum amount of retries */
-#define WIFI_CONNECTED_BIT BIT0
-#define WIFI_FAIL_BIT      BIT1
+static esp_err_t i2c_master_init(void) {
+    i2c_config_t conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .scl_io_num = I2C_MASTER_SCL_IO,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = I2C_MASTER_FREQ_HZ,
+    };
+    i2c_param_config(I2C_MASTER_NUM, &conf);
+    return i2c_driver_install(I2C_MASTER_NUM, conf.mode,
+                              I2C_MASTER_RX_BUF_DISABLE,
+                              I2C_MASTER_TX_BUF_DISABLE, 0);
+}
 
-void wifi_init_softap(void);
-void wifi_init_sta(void);
-static void event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
+static esp_err_t lcd_send_cmd(char cmd) {
+    esp_err_t err;
+    char data_u, data_l;
+    uint8_t data_t[4];
+    data_u = (cmd & 0xf0);
+    data_l = ((cmd << 4) & 0xf0);
+    data_t[0] = data_u | 0x0C; // en=1, rs=0
+    data_t[1] = data_u | 0x08; // en=0, rs=0
+    data_t[2] = data_l | 0x0C; // en=1, rs=0
+    data_t[3] = data_l | 0x08; // en=0, rs=0
 
-static const char *TAG = "wifi station";
+    err = i2c_master_write_to_device(I2C_MASTER_NUM, SLAVE_ADDRESS_LCD, data_t, 4, 1000);
+    if (err != ESP_OK) ESP_LOGI(TAG, "Error no. %d in command", err);
+    return err;
+}
 
-static int s_retry_num = 0;
+static esp_err_t lcd_send_data(char data) {
+    esp_err_t err;
+    char data_u, data_l;
+    uint8_t data_t[4];
+    data_u = (data & 0xf0);
+    data_l = ((data << 4) & 0xf0);
+    data_t[0] = data_u | 0x0D; // en=1, rs=1
+    data_t[1] = data_u | 0x09; // en=0, rs=1
+    data_t[2] = data_l | 0x0D; // en=1, rs=1
+    data_t[3] = data_l | 0x09; // en=0, rs=1
 
-static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
-    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
-        esp_wifi_connect();
-    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-        if (s_retry_num < CONFIG_ESP_MAXIMUM_RETRY) {
-            esp_wifi_connect();
-            s_retry_num++;
-            ESP_LOGI(TAG, "retry to connect to the AP");
-        } else {
-            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
-        }
-        ESP_LOGI(TAG,"connect to the AP fail");
-    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
-        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
-        s_retry_num = 0;
-        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
-    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_AP_STACONNECTED) {
-        wifi_event_ap_staconnected_t *event = (wifi_event_ap_staconnected_t *) event_data;
-        ESP_LOGI(TAG, "station " MACSTR " joined, AID=%d", MAC2STR(event->mac), event->aid);
-    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_AP_STADISCONNECTED) {
-        wifi_event_ap_stadisconnected_t *event = (wifi_event_ap_stadisconnected_t *) event_data;
-        ESP_LOGI(TAG, "station " MACSTR " left, AID=%d, reason:%d", MAC2STR(event->mac), event->aid, event->reason);
+    err = i2c_master_write_to_device(I2C_MASTER_NUM, SLAVE_ADDRESS_LCD, data_t, 4, 1000);
+    if (err != ESP_OK) ESP_LOGI(TAG, "Error no. %d in command", err);
+    return err;
+}
+
+static void lcd_init(void) {
+   usleep(50000);
+    lcd_send_cmd(0x30);
+   usleep(5000);
+    lcd_send_cmd(0x30);
+   usleep(200);
+    lcd_send_cmd(0x30);
+   usleep(10000);
+    lcd_send_cmd(0x20); // 4-bit mode
+   usleep(10000);
+
+    // display initialization
+    lcd_send_cmd(0x28); // Function set: 4-bit mode, 2 lines, 5x8 dots
+   usleep(1000);
+    lcd_send_cmd(0x08); // Display off
+   usleep(1000);
+    lcd_send_cmd(0x01); // Clear display
+   usleep(1000);
+   usleep(1000);
+    lcd_send_cmd(0x06); // Entry mode: increment cursor, no shift
+   usleep(1000);
+    lcd_send_cmd(0x0C); // Display on, cursor off, blink off
+   usleep(1000);
+}
+
+static void lcd_send_string(char *str) {
+    while (*str) lcd_send_data(*str++);
+}
+
+void lcd_clear(void){
+    lcd_send_cmd(0x80);
+    for (int i = 0; i < 70; i++){
+        lcd_send_data(' ');
     }
 }
 
-void wifi_init_softap(void) {
-    esp_netif_create_default_wifi_ap();
-
-    wifi_config_t wifi_config = {
-        .ap = {
-            .ssid = CONFIG_SOFTAP_SSID,
-            .ssid_len = strlen(CONFIG_SOFTAP_SSID),
-            .password = CONFIG_SOFTAP_PASSWORD,
-            .channel = CONFIG_SOFTAP_CHANNEL,
-            .max_connection = CONFIG_MAX_STA_CONN,
-            .authmode = WIFI_AUTH_WPA2_PSK
-        },
-    };
-
-    if (strlen(CONFIG_SOFTAP_PASSWORD) == 0) {
-        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+void lcd_put_cur(int row, int col)
+{
+    switch (row)
+    {
+        case 0:
+            col |= 0x80;
+            break;
+        case 1:
+            col |= 0xC0;
+            break;
     }
 
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
-    ESP_ERROR_CHECK(esp_wifi_start());
-
-    ESP_LOGI(TAG, "wifi_init_softap finished. SSID:%s password:%s",
-             CONFIG_SOFTAP_SSID, CONFIG_SOFTAP_PASSWORD);
-}
-
-void wifi_init_sta(void) {
-    s_wifi_event_group = xEventGroupCreate();
-
-    ESP_ERROR_CHECK(esp_netif_init());
-
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_sta();
-
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-    esp_event_handler_instance_t instance_any_id;
-    esp_event_handler_instance_t instance_got_ip;
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
-                                                        ESP_EVENT_ANY_ID,
-                                                        &event_handler,
-                                                        NULL,
-                                                        &instance_any_id));
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
-                                                        IP_EVENT_STA_GOT_IP,
-                                                        &event_handler,
-                                                        NULL,
-                                                        &instance_got_ip));
-
-    wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = CONFIG_ESP_WIFI_SSID,
-            .password = CONFIG_ESP_WIFI_PASSWORD,
-            .threshold.authmode = WIFI_AUTH_WPA2_PSK,
-        },
-    };
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
-    ESP_ERROR_CHECK(esp_wifi_start() );
-
-    ESP_LOGI(TAG, "wifi_init_sta finished.");
-
-    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
-            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
-            pdFALSE,
-            pdFALSE,
-            portMAX_DELAY);
-
-    if (bits & WIFI_CONNECTED_BIT) {
-        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
-                 CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASSWORD);
-    } else if (bits & WIFI_FAIL_BIT) {
-        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
-                 CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASSWORD);
-        // Start SoftAP
-        ESP_LOGI(TAG, "Starting SoftAP");
-        wifi_init_softap();
-    } else {
-        ESP_LOGE(TAG, "UNEXPECTED EVENT");
-    }
+    lcd_send_cmd (col);
 }
 
 void app_main(void) {
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-      ESP_ERROR_CHECK(nvs_flash_erase());
-      ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
+    ESP_ERROR_CHECK(i2c_master_init());
+    ESP_LOGI(TAG, "I2C initialized successfully");
 
-    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
-    wifi_init_sta();
+    lcd_init();
+    ESP_LOGI(TAG, "LCD initialized successfully");
+
+    lcd_send_cmd(0x80); // Move cursor to the beginning of the first line
+    lcd_send_string("Hello World!");
+
+    lcd_send_cmd(0xC0); // Move cursor to the beginning of the second line
+    lcd_send_string("from ESP32");
 }
